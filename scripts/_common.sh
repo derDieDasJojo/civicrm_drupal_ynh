eb8d5d87348c5ed7ce05b559df6a2dd097780e08
jojo <jojo>
2024-10-31 18:26:53 +0000

    gWIP: update to D10

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,15 +3,23 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-
+#phpversion=8.3
+#php_version=8.3
+#YNH_COMPOSER_VERSION=2.8.1
+composer_version=2.8.1
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
-_ynh_exec_with_drush_php() {
-    ynh_exec_warn_less ynh_exec_as "$app" \
-        env PATH="$PATH" DRUSH_PHP="/usr/bin/php$phpversion" \
-        "$@"
+#_ynh_exec_with_drush_php() {
+#    ynh_exec_warn_less ynh_exec_as "$app" \
+#        env PATH="$PATH" DRUSH_PHP="/usr/bin/php$phpversion" \
+#        "$@"
+#}
+_ynh_exec_drush() {
+    ynh_hide_warnings ynh_exec_as_app \
+        env PATH="$PATH" DRUSH_PHP="/usr/bin/php$php_version" \
+        ./vendor/drush/drush/drush "$@"
 }
 
 #=================================================
